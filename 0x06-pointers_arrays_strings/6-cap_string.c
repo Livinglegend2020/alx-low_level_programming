f1ee03df88b842def6d686077b1f8db84a650a40
Abdulrauf Bayo <florider2020@gmail.com>
2022-07-14 14:05:39 -0700

    6-cap_string.c is present

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,19 @@
-#include "main.h"
-/**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
- */
+#include <stdio.h>
 
-char *cap_string(char *s)
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ * only using putchar and without char variables.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-	
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
-	}
-	return (s);
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
 }
