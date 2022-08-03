e29c7b669db49d71ed8efb58323ac75b6bf25ade
livinglegend2020 <florider2020@gmail.com>
2022-08-03 06:13:05 -0700

    libmy

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: Length.
+ */
+size_t _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str++i)
+		len++;
+
+	return (len);
+}
